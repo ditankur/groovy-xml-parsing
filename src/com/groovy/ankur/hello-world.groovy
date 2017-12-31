bd81fb250c49ecd8fd7b8aad47b40a0396f3afca
LAPTOP-GHAC7N8O\ankur <ankur.or.sharma@gmail.com>
2017-12-31 17:46:37 +0000

    Groovy xml parsing and writing data to another file

---
+++ src/com/groovy/ankur/hello-world.groovy
@@ -0,0 +1,29 @@
+package com.groovy.ankur
+
+def COMMA_SEPARATOR = ","
+def NEW_LINE = "\n"
+def folder = new File("C:\\Users\\ankur\\IdeaProjects\\groovy-demos")
+def fileToWrite = new File("C:\\Users\\ankur\\IdeaProjects\\groovy-demos\\books.csv")
+def books
+println(folder.listFiles())
+fileToWrite.withWriter {
+    folder.listFiles().findAll { file -> file.getName().startsWith("books-") }.each { file ->
+        println(file.getName())
+        books = new XmlParser().parse(file)
+        books.children().each { row ->
+            it.append(COMMA_SEPARATOR)
+            it.append(row.name.text())
+            it.append(COMMA_SEPARATOR)
+            it.append(row.author.text())
+            it.append(COMMA_SEPARATOR)
+            it.append(row.version.text())
+            it.append(NEW_LINE)
+
+        }
+    }
+}
+
+
+
+
+
